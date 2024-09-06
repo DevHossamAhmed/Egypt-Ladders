de30b088ee8e0e885dad0575ffae495bb47e8ccd
DevHossamAhmed <dev.hossamelden@gmail.com>
2024-09-06 16:45:56 +0300

    update

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -4,10 +4,9 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import styles from "./slide.module.css";
 import "@splidejs/splide/dist/css/splide.min.css";
 import Image from "next/image";
-import slide01 from "../../../public/images/311571751_104132715819610_3555219688868392360_n.jpg";
-import slide02 from "../../../public/images/311571751_104132715819610_3555219688868392360_n.jpg";
-import slide03 from "../../../public/images/407483866_316287071270839_1553674601491804216_n.jpg";
-import slide04 from "../../../public/images/419169580_340351588864387_2396803983930841681_n.jpg";
+import slide01 from "../../../public/images/slider01.png";
+import slide02 from "../../../public/images/slider02.png";
+
 import slide05 from "../../../public/images/home-about.jpg";
 
 const Slide = () => {
@@ -29,12 +28,6 @@ const Slide = () => {
         </SplideSlide>
         <SplideSlide className={styles.secPic}>
           <Image src={slide02} alt="Image 2" className={styles.imgeSlide}/>
-        </SplideSlide>
-        <SplideSlide className={styles.therPic}>
-          <Image src={slide03} alt="Image 3" className={styles.imgeSlide}/>
-        </SplideSlide>
-        <SplideSlide className={styles.fourPic}>
-          <Image src={slide04} alt="Image 4" className={styles.imgeSlide}/>
         </SplideSlide>
         <SplideSlide className={styles.fivPic}>
           <Image src={slide05} alt="Image 5" className={styles.imgeSlide}/>
